9dbf877df9e2e67193fff52dd19b91f550741643
Junghwan <balance139@gmail.com>
2023-05-27 13:27:18 +0900

    23.05.27_4

--- src/test/java/com/example/Spring_board/author/repository/AuthorRepositoryTest.java
+++ src/test/java/com/example/Spring_board/author/repository/AuthorRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.Spring_board.author.repository;
 
 import com.example.Spring_board.author.domain.Author;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -29,7 +30,9 @@ public class AuthorRepositoryTest {
         authorInput.setEmail("hong@naver.com");
         authorInput.setPassword("1234");
 
+
         authorRepository.save(authorInput);
+
 
 //        저장된 데이터를 다시 조회해서, 입력한 테스트 데이터와 동일한지 검증
 //        조회를 해야 하는데, id를 모르므로 기본 메서드인 findById를 사용할수가 없으므로,
@@ -39,9 +42,16 @@ public class AuthorRepositoryTest {
 
 //        input값과 조회한 값이 같은지를 비교해 보는 방법
 //        방법1 : print 해 본다.
-        System.out.println("name : " + authorDB.getName());
-        System.out.println("Email : " + authorDB.getEmail());
-        System.out.println("Password : " + authorDB.getPassword());
+//        System.out.println("name : " + authorDB.getName());
+//        System.out.println("Email : " + authorDB.getEmail());
+//        System.out.println("Password : " + authorDB.getPassword());
+        
+//        방법2 : 실행 시 에러가 나면 빨간불, 정상이면 파란불로 구분할 수 있도록 
+//        JUnit의 기능 활용
+//        Junit이란 java/spring에서 테스트 용도의 tool로 가장 많이 사용되는 라이브러리
+
+        Assertions.assertEquals(authorInput,authorDB);
+
 
 
 
