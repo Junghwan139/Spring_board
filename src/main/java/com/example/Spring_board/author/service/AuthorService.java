303601299368ccac789381e862e575de093786dc
Junghwan Lim <balance139@gmail.com>
2023-06-04 19:43:22 +0900

    23.06.04_2

--- src/main/java/com/example/Spring_board/author/service/AuthorService.java
+++ src/main/java/com/example/Spring_board/author/service/AuthorService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Collections;
 import java.util.List;
 
