cc34e8ba51ceaac1d487d11ade05a07ffec9e134
Junghwan Lim <balance139@gmail.com>
2023-06-04 15:50:06 +0900

    23.06.04

--- src/main/java/com/example/Spring_board/study/S1/s1_post/s1PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Spring_board.study.S1.s1_post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface s1PostRepository extends JpaRepository<s1Post, Long> {
-
-}
--- src/main/java/com/example/Spring_board/study/S1/s1_post/s1PostRequestDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.Spring_board.study.S1.s1_post;
-
-public class s1PostRequestDto {
-}
