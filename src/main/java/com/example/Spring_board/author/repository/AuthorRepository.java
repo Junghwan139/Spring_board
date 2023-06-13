776ace8cb2c9b127fbb004fc59697dee07e5d75a
Junghwan Lim <balance139@gmail.com>
2023-06-14 07:36:12 +0900

    23.06.14

--- src/main/java/com/example/Spring_board/author/repository/AuthorRepository.java
+++ src/main/java/com/example/Spring_board/author/repository/AuthorRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 //    findByA라는 규칙으로 jpa에서는 자동으로 where 조건문으로 a를 사용하게 된다.
-//    findByAandB : A와 B를 and조건으로 where문에서 조회할 때 사용
+//    findByAandB : A와 B를 and조건으로 where문에서 조회할 때 사용 A, B의 첫 문자는 대문자
     Author findByEmail(String myEmail);
 
 }
